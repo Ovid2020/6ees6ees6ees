fc11b96556e4534e193bc4f3bd78218b7739c320
Vincent Barilla <vincentbarilla@Vincents-MacBook-Pro.local>
2016-08-07 22:28:59 -0700

    ES6 implementation for HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,6 +1,18 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
+	constructor(age, color, food, job){
+		super(age || 10, color, food, job || 'make honey');
+		this.honeyPot = 0;
+	}
+
+	makeHoney(){
+		this.honeyPot++;
+	}
+
+	giveHoney(){
+		this.honeyPot--;
+	}
   // TODO..
 };
 
